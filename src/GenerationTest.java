2b3828ce69eabd4673726a1be701e297dda273bc
jini29 <60157945+jini29@users.noreply.github.com>
2020-04-12 21:27:10 -0500

    Created JUnit test for generation class ensures that all the methods in the generation works as expected.

---
+++ src/GenerationTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GenerationTest {
+
+	
+		
+		CellState CellStates[] = {CellState.ON,CellState.OFF,CellState.ON, CellState.OFF,
+				CellState.ON,CellState.OFF,CellState.ON,CellState.OFF};
+		Generation g1 = new Generation(CellStates);
+		
+		Cell Cells[] = {new Cell(CellState.ON),new Cell(CellState.OFF),new Cell(CellState.ON), new Cell(CellState.OFF),
+				new Cell(CellState.ON),new Cell(CellState.OFF),new Cell(CellState.ON),new Cell(CellState.OFF)};
+		
+		Generation g2 = new Generation(Cells);
+		
+		String states = ".o.o.o.o";
+		Generation g3 = new Generation(string);
+	
+	@Test
+	public void sizeTest() {
+		int expect = 8;
+		int actual = g1.size();
+		assertEquals(expect,actual);
+		
+		actual = g2.size();
+		assertEquals(expect,actual);
+		
+		actual = g3.size();
+		assertEquals(expect,actual);
+		
+	}
+	
+	@Test
+	public void getCellTest() {
+		CellState expect = CellState.OFF;
+		CellState actual = g1.getCell(2).getState();
+		assertEquals(expect,actual);
+		
+		actual = g2.getCell(2).getState();
+		assertEquals(expect,actual);
+	
+		actual = g3.getCell(2).getState();
+		assertEquals(expect,actual);
+		
+		expect = CellState.ON;
+		actual = g1.getCell(3).getState();
+		assertEquals(expect,actual);
+		
+		actual = g2.getCell(3).getState();
+		assertEquals(expect,actual);
+		
+		actual = g3.getCell(3).getState();
+		assertEquals(expect,actual);
+	}
+	
+	@Test
+	public void toStringTest() {
+		String expect = ".o.o.o.o";
+		String actual = g1.toString();
+		assertEquals(expect,actual);
+		
+		actual = g2.toString();
+		assertEquals(expect,actual);
+	
+		actual = g3.toString();
+		assertEquals(expect,actual);
+	}
+
+}
