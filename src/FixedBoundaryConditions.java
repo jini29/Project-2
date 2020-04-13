b19063ae6d4e193e4a6a6c9846a6c28e4c3eb69b
jini29 <60157945+jini29@users.noreply.github.com>
2020-04-12 23:39:46 -0500

    define all the methods in FixedBounaryCondition class and passed all the Junit test to ensure it works as expected.

--- src/FixedBoundaryConditions.java
+++ src/FixedBoundaryConditions.java
@@ -10,17 +10,28 @@ public class FixedBoundaryConditions implements BoundaryConditions {
 	}
 	
 	public CellState getLeftState() {
-		return null;
+		return this.left;
 	}
 	
 	public CellState getRightState() {
-		return null;
+		return this.right;
 	}
 	@Override
 	public Cell getNeighbor(int cellIdx, int offset, Generation gen) {
 		// TODO Auto-generated method stub
-		return null;
+		int NeighborIdx = cellIdx + offset;
+		if (NeighborIdx > 0 && NeighborIdx < gen.size()) {
+			return gen.getCell(NeighborIdx);
+		}
+		else if(NeighborIdx < 0){
+			return new Cell(getLeftState());
+		}
+		else {
+			return new Cell(getRightState());
+		}
 	}
 	
-	
 }
+	
+	
+
