d79fd6019e5c74577b1cb5e2e4fae1e3445ff38b
jini29 <60157945+jini29@users.noreply.github.com>
2020-04-21 14:07:37 -0500

    change the get neigbor methods in which the method can now get the neigbor correctly for offset that is longer than the size of the generation.

--- src/CircularBoundaryConditions.java
+++ src/CircularBoundaryConditions.java
@@ -7,16 +7,22 @@ public class CircularBoundaryConditions implements BoundaryConditions{
 	@Override
 	public Cell getNeighbor(int cellIdx, int offset, Generation gen) {
 		// TODO Auto-generated method stub
+	
 		int NeighborIdx = cellIdx + offset;
-		if (NeighborIdx >= 0 && NeighborIdx < gen.size()) {
-			return gen.getCell(NeighborIdx);
-		}
-		else if(NeighborIdx < 0){
-			return gen.getCell(gen.size()+NeighborIdx);
+		 if(NeighborIdx < 0){
+			while(NeighborIdx < 0)
+			NeighborIdx = gen.size()+NeighborIdx;
 		}
 		else {
-			return gen.getCell(NeighborIdx-gen.size());
+			while(NeighborIdx >= gen.size()) {
+				NeighborIdx = NeighborIdx-gen.size();
+			}
+			
 		}
+		
+		return gen.getCell(NeighborIdx);
 	}
+	
+
 
 }
